d81eddd898621ada280538ce06169f8aeb643296
Alex Prascak <aprascak@ufl.edu>
2020-07-14 09:59:17 -0400

    Updates

---
+++ precache-manifest.7ea08224793209e838aace0acfd4dcf3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9f86595e6684d5f60c7820ec090dba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5ba1ca4f197c821dac8",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4082995cb5a0b020245f",
+    "url": "/static/css/main.6b458943.chunk.css"
+  },
+  {
+    "revision": "b5ba1ca4f197c821dac8",
+    "url": "/static/js/2.0bc05905.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.0bc05905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4082995cb5a0b020245f",
+    "url": "/static/js/main.d38ad150.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "3bf6e6aec5c98f0ee65ab063da3005a9",
+    "url": "/static/media/Prascak_Resume.3bf6e6ae.pdf"
+  },
+  {
+    "revision": "0c246fa08e8969160ef2f65ef1cc4776",
+    "url": "/static/media/resume.0c246fa0.pdf"
+  },
+  {
+    "revision": "106cf67e58157dd4961c756d1eb5900b",
+    "url": "/static/media/shouldwork.106cf67e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.1bc656d2a3b7f21f8e712a0e0873d43b.js"
+  "/precache-manifest.7ea08224793209e838aace0acfd4dcf3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
